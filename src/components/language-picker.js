cc50a0ce6d0495d79950e1bd40395ec5d47d55a1
marcin-flad <marcin.fladro@matrix-reliability.eu>
2024-04-25 11:38:01 +0200

    fix language selection on pages which contain language in path

--- src/components/language-picker.js
+++ src/components/language-picker.js
@@ -1,11 +1,19 @@
+import i18next from 'i18next';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { navigate } from "gatsby";
 
 export default function LanguagePicker() {
     const { i18n } = useTranslation();
-  
+    const currentUrl = window.location.href;
+    const currentLanguage = i18next.language
+
     const changeLanguage = (e) => {
       i18n.changeLanguage(e.target.value);
+      const urlLanguageSegment = `/${currentLanguage}/`
+      if(currentUrl.endsWith(urlLanguageSegment)){
+        navigate(currentUrl.replace(urlLanguageSegment, `/${e.target.value}/`))
+      }
     };
   
     return (
